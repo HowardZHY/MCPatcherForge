eb82a97bb1f239408df89bdb06e3f0f01bc147af
Howard ZHY <howardzhy128@163.com>
2023-12-19 18:36:27 +0800

    Fix mixin method srg name

--- src/main/java/mist475/mcpatcherforge/mixins/optifine/MixinItemRendererOF.java
+++ src/main/java/mist475/mcpatcherforge/mixins/optifine/MixinItemRendererOF.java
@@ -1,6 +1,5 @@
 package mist475.mcpatcherforge.mixins.optifine;
 
-import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import com.prupe.mcpatcher.cit.CITUtils;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
@@ -16,21 +15,41 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class MixinItemRendererOF {
 
     @Redirect(
-        method = "renderItem(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/item/ItemStack;I)V",
+        method = "func_78443_a(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/item/ItemStack;I)V",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/entity/EntityLivingBase;getItemIcon(Lnet/minecraft/item/ItemStack;I)Lnet/minecraft/util/IIcon;"
-        ),
-        remap = false)
-    private IIcon modifyRenderItem(EntityLivingBase instance, ItemStack item, int renderPass, EntityLivingBase entity, ItemStack item2, int renderPass1) {
+            target = "Lnet/minecraft/entity/EntityLivingBase;func_70620_b(Lnet/minecraft/item/ItemStack;I)Lnet/minecraft/util/IIcon;"
+        )
+    )
+    private IIcon modifyRenderItem(EntityLivingBase elb, ItemStack item, int renderPass, EntityLivingBase entity, ItemStack item2, int renderPass1) {
         return CITUtils.getIcon(entity.getItemIcon(item2, renderPass1), item2, renderPass1);
     }
 
     @Redirect(
-        method = "renderItem(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/item/ItemStack;ILnet/minecraftforge/client/IItemRenderer$ItemRenderType;)V",
-        at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;hasEffect()Z"),
-        remap = false)
+        method = "renderItem(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/item/ItemStack;I)V",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/entity/EntityLivingBase;getItemIcon(Lnet/minecraft/item/ItemStack;I)Lnet/minecraft/util/IIcon;" //func_70620_b
+        )
+    )
+    private IIcon modifyRenderItemDev(EntityLivingBase elb, ItemStack item, int renderPass, EntityLivingBase entity, ItemStack item2, int renderPass1) {
+        return CITUtils.getIcon(entity.getItemIcon(item2, renderPass1), item2, renderPass1);
+    }
+
+    @Redirect(
+        method = "func_78443_a(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/item/ItemStack;I)V",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;func_77962_s()Z")
+    )
     private boolean modifyRenderItemGlint(ItemStack item, EntityLivingBase entity, ItemStack itemStack, int renderPass) {
         return !CITUtils.renderEnchantmentHeld(item, renderPass) && item.hasEffect(renderPass);
     }
+
+    @Redirect(
+        method = "renderItem(Lnet/minecraft/entity/EntityLivingBase;Lnet/minecraft/item/ItemStack;I)V",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;hasEffect()Z") //func_77962_s
+    )
+    private boolean modifyRenderItemGlintDev(ItemStack item, EntityLivingBase entity, ItemStack itemStack, int renderPass) {
+        return !CITUtils.renderEnchantmentHeld(item, renderPass) && item.hasEffect(renderPass);
+    }
+    
 }
