b8018f7d08cfe1a628a42f69b11e25de7d37761f
Howard ZHY <howardzhy128@163.com>
2025-09-09 00:34:17 +0800

    Transform FML to avoid class loading issues

--- src/main/java/mist475/mcpatcherforge/core/CITForgeCore.java
+++ src/main/java/mist475/mcpatcherforge/core/CITForgeCore.java
@@ -1,7 +1,9 @@
 package mist475.mcpatcherforge.core;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.Launch;
+import net.minecraft.launchwrapper.LaunchClassLoader;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.MixinEnvironment;
 
@@ -10,19 +12,39 @@ import java.lang.reflect.Field;
 import java.net.*;
 import java.util.*;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "unchecked"})
 @IFMLLoadingPlugin.TransformerExclusions({"mist475.mcpatcherforge.core", "mist475.mcpatcherforge.mixins"})
 @IFMLLoadingPlugin.SortingIndex(Integer.MIN_VALUE + 2)
 public class CITForgeCore implements IFMLLoadingPlugin {
 
-    public CITForgeCore() {}
-
-    static {
+    public CITForgeCore() {
         fixMixinClasspathOrder();
+        try {
+            Field f = LaunchClassLoader.class.getDeclaredField("transformerExceptions");
+            f.setAccessible(true);
+            Set<String> exs = (Set<String>) f.get(Launch.classLoader);
+            System.out.println(exs);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        injectMixinTweaker();
         MixinBootstrap.init();
         MixinEnvironment.getDefaultEnvironment().setSide(MixinEnvironment.Side.CLIENT);
-        org.spongepowered.asm.mixin.Mixins.addConfiguration("mixins.cit.client.json");
-        org.spongepowered.asm.mixin.Mixins.addConfiguration("mixins.cit.optifine.json");
+    }
+
+    private static void injectMixinTweaker() {
+        String tweakClass = "org.spongepowered.asm.launch.MixinTweaker";
+        List<String> tweakClasses = (List<String>) Launch.blackboard.get("TweakClasses");
+        List<ITweaker> tweakers = (List<ITweaker>) Launch.blackboard.get("Tweaks");
+        if (tweakClasses.contains(tweakClass)) {
+            return;
+        }
+        for (ITweaker existingTweaker : tweakers) {
+            if (tweakClass.equals(existingTweaker.getClass().getName())) {
+                return;
+            }
+        }
+        tweakClasses.add(tweakClass);
     }
 
     private static void fixMixinClasspathOrder() {
@@ -54,6 +76,9 @@ public class CITForgeCore implements IFMLLoadingPlugin {
 
     @Override
     public String[] getASMTransformerClass() {
+        Launch.classLoader.registerTransformer("mist475.mcpatcherforge.core.FMLTransformer");
+        org.spongepowered.asm.mixin.Mixins.addConfiguration("mixins.cit.client.json");
+        org.spongepowered.asm.mixin.Mixins.addConfiguration("mixins.cit.optifine.json");
         return new String[]{"mist475.mcpatcherforge.core.CITAccessTransformer"};
     }
 
